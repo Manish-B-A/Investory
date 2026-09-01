3ee3d9c4c677066cfba6e5bb445b565ea4c26833
Manish <manish.kumar1@thetalentpool.ai>
2026-09-01 15:43:31 +0530

    supabase keys;

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  supabaseUrl: '',
-  supabaseAnonKey: '',
+  supabaseUrl: 'https://xfvobiupsvhawqggigje.supabase.co',
+  supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inhmdm9iaXVwc3ZoYXdxZ2dpZ2plIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODgyNDk3NzcsImV4cCI6MjEwMzgyNTc3N30.VuCEODRXso4RrlZqWTpkNks_TAZGGbmR5LtvCbR8yu8',
 };
